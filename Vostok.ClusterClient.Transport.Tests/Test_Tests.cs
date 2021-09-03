fd0324b7b1755186381f9f91b521832798bd61b6
kungurtsev <kungurtsev@skbkontur.ru>
2021-09-03 12:57:59 +0500

    + Test_Tests

---
+++ Vostok.ClusterClient.Transport.Tests/Test_Tests.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Logging.Abstractions;
+using Vostok.Logging.Console;
+
+namespace Vostok.Clusterclient.Transport.Tests
+{
+    [TestFixture]
+    public class Test_Tests
+    {
+        [Test]
+        public void Good()
+        {
+            new SynchronousConsoleLog().Info("info");
+            new SynchronousConsoleLog().Warn("warn");
+            new SynchronousConsoleLog().Error("error");
+        }
+        
+        [Test]
+        public void Bad()
+        {
+            new SynchronousConsoleLog().Info("info");
+            new SynchronousConsoleLog().Warn("warn");
+            new SynchronousConsoleLog().Error("error");
+            1.Should().Be(2);
+        }
+    }
+}
