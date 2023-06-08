07560b52068ee3bb3620bd8550d02b711a67126a
deniaa <deniaa@skbkontur.ru>
2023-06-08 22:15:35 +0500

    Due to significant changes in System.Net.Http.HttpConnection class SocketTuningContent can't work starting from .Net7. But starting from .Net5 HttpMessageHandler has a special api for setting TcpKeepAlive options. See in NetCore50Utils.TuneHandler and SocketsHttpHandlerTuner.

--- Vostok.ClusterClient.Transport.Core50/SocketsHttpHandlerTuner.cs
+++ Vostok.ClusterClient.Transport.Core50/SocketsHttpHandlerTuner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Text;
 using JetBrains.Annotations;
 
@@ -11,10 +12,37 @@ namespace Vostok.Clusterclient.Transport.Core50
         private static readonly Encoding HeadersEncoding = new UTF8Encoding(false);
 
         [UsedImplicitly]
-        public static void Tune(HttpMessageHandler handler)
+        public static void Tune(HttpMessageHandler handler, bool tcpKeepAliveEnables, TimeSpan tcpKeepAliveInterval, TimeSpan tcpKeepAliveTime)
         {
             if (!(handler is SocketsHttpHandler socketHandler))
                 return;
+
+            if (tcpKeepAliveEnables)
+            {
+                var keepAliveTime = (int)tcpKeepAliveTime.TotalSeconds;
+                var keepAliveInterval = (int)tcpKeepAliveInterval.TotalSeconds;
+                
+                socketHandler.ConnectCallback = async (context, token) =>
+                {
+                    var socket = new Socket(SocketType.Stream, ProtocolType.Tcp) {NoDelay = true};
+                    try
+                    {
+                        socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+                        socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, keepAliveTime);
+                        socket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, keepAliveInterval);
+                        // (deniaa, 08.06.2023): We can configure an option (SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveRetryCount, X) here, but we have no this option in transport settings model.
+
+                        await socket.ConnectAsync(context.DnsEndPoint, token).ConfigureAwait(false);
+
+                        return new NetworkStream(socket, ownsSocket: true);
+                    }
+                    catch
+                    {
+                        socket.Dispose();
+                        throw;
+                    }
+                };
+            }
 
             socketHandler.RequestHeaderEncodingSelector = (_, __) => HeadersEncoding;
             socketHandler.ResponseHeaderEncodingSelector = (_, __) => HeadersEncoding;
