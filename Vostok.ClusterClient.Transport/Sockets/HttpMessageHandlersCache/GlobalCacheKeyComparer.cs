e535801370b010e53ce565416965916905c09960
gladysheva <gladysheva@skbkontur.ru>
2024-11-05 13:32:05 +0500

    Refactor

---
+++ Vostok.ClusterClient.Transport/Sockets/HttpMessageHandlersCache/GlobalCacheKeyComparer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Vostok.Clusterclient.Transport.Sockets.HttpMessageHandlersCache;
+
+internal class GlobalCacheKeyComparer : IEqualityComparer<GlobalCacheKey>
+{
+    public static readonly GlobalCacheKeyComparer Instance = new GlobalCacheKeyComparer();
+
+    public bool Equals(GlobalCacheKey x, GlobalCacheKey y)
+    {
+        return
+            ReferenceEquals(x.Proxy, y.Proxy) &&
+            ReferenceEquals(x.ClientCertificates, y.ClientCertificates) &&
+            x.AllowAutoRedirect == y.AllowAutoRedirect &&
+            x.ConnectionTimeout == y.ConnectionTimeout &&
+            x.ConnectionIdleTimeout == y.ConnectionIdleTimeout &&
+            x.ConnectionLifetime == y.ConnectionLifetime &&
+            x.MaxConnectionsPerEndpoint == y.MaxConnectionsPerEndpoint &&
+            x.RemoteCertificateValidationCallback == y.RemoteCertificateValidationCallback &&
+            Equals(x.Credentials, y.Credentials) &&
+            x.DecompressionMethods == y.DecompressionMethods &&
+            x.TcpKeepAliveEnables == y.TcpKeepAliveEnables &&
+            x.TcpKeepAliveInterval == y.TcpKeepAliveInterval &&
+            x.TcpKeepAliveTime == y.TcpKeepAliveTime;
+    }
+
+    public int GetHashCode(GlobalCacheKey key)
+    {
+        unchecked
+        {
+            var hashCode = key.Proxy != null ? key.Proxy.GetHashCode() : 0;
+            hashCode = (hashCode * 397) ^ key.AllowAutoRedirect.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.ConnectionTimeout.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.ConnectionIdleTimeout.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.ConnectionLifetime.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.MaxConnectionsPerEndpoint;
+            hashCode = (hashCode * 397) ^ (key.ClientCertificates != null ? key.ClientCertificates.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ (key.RemoteCertificateValidationCallback != null ? key.RemoteCertificateValidationCallback.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ (key.Credentials != null ? key.Credentials.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ key.DecompressionMethods.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.TcpKeepAliveEnables.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.TcpKeepAliveInterval.GetHashCode();
+            hashCode = (hashCode * 397) ^ key.TcpKeepAliveTime.GetHashCode();
+            return hashCode;
+        }
+    }
+}
---
+++ Vostok.ClusterClient.Transport/Sockets/HttpMessageHandlersCache/SocketsHandlerProvider.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using Vostok.Clusterclient.Transport.Helpers;
+using Vostok.Commons.Collections;
+using Vostok.Commons.Environment;
+
+namespace Vostok.Clusterclient.Transport.Sockets.HttpMessageHandlersCache
+{
+    internal class SocketsHandlerProvider
+    {
+        private const int GlobalCacheCapacity = 25;
+        private const int LocalCacheCapacity = 3;
+
+        // (iloktionov): Global cache is limited in size to prevent leaks resulting from random connection timeouts:
+        private static readonly RecyclingBoundedCache<GlobalCacheKey, HttpMessageHandler> globalCache
+            = new RecyclingBoundedCache<GlobalCacheKey, HttpMessageHandler>(GlobalCacheCapacity, GlobalCacheKeyComparer.Instance);
+
+        // (iloktionov): Local cache isolates well-behaving clients from others who cause global cache purging:
+        private readonly RecyclingBoundedCache<TimeSpan, HttpMessageHandler> localCache;
+        private readonly Func<TimeSpan, HttpMessageHandler> localCacheFactory;
+
+        public SocketsHandlerProvider(SocketsTransportSettings settings)
+        {
+            localCache = new RecyclingBoundedCache<TimeSpan, HttpMessageHandler>(LocalCacheCapacity);
+            localCacheFactory = timeout => globalCache.Obtain(GlobalCacheKey.Create(settings, timeout), CreateHandler);
+        }
+
+        public HttpMessageHandler Obtain(TimeSpan? connectionTimeout)
+            => localCache.Obtain(connectionTimeout ?? Timeout.InfiniteTimeSpan, localCacheFactory);
+
+        private static HttpMessageHandler CreateHandler(GlobalCacheKey key)
+        {
+            var handler = NetCore21Utils.CreateSocketsHandler(
+                key.Proxy,
+                key.AllowAutoRedirect,
+                key.ConnectionTimeout,
+                key.ConnectionIdleTimeout,
+                key.ConnectionLifetime,
+                key.MaxConnectionsPerEndpoint,
+                key.ClientCertificates,
+                key.RemoteCertificateValidationCallback,
+                key.Credentials,
+                key.DecompressionMethods);
+
+            if (RuntimeDetector.IsDotNet50AndNewer)
+                NetCore50Utils.TuneHandler(handler, key.TcpKeepAliveEnables, key.TcpKeepAliveInterval, key.TcpKeepAliveTime);
+
+            return handler;
+        }
+    }
+}
