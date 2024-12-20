34411104d81a1556f03fc3286600d70c8ad0c170
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2024-12-20 17:51:55 +0200

    Disable Http native tracing creation.

---
+++ Vostok.ClusterClient.Transport/Helpers/NetCore60Utils.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Http;
+
+namespace Vostok.Clusterclient.Transport.Helpers;
+
+internal static class NetCore60Utils
+{
+    public static void TuneHandler(HttpMessageHandler handler)
+    {
+#if NET6_0_OR_GREATER
+        if (handler is not SocketsHttpHandler socketsHandler)
+            return;
+
+        // note (ponomaryovigor):
+        // Disable Http Activity native creation because ClusterClient implements tracing manually.
+        socketsHandler.ActivityHeadersPropagator = null;
+#endif
+    }
+}
